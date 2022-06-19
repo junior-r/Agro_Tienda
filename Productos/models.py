3a071a53c23c956087d1f6fde9de312c585b5323
Junior Ruiz <junior31064049@gmail.com>
2022-06-19 13:22:02 -0400

    Nueva función para guardar las imagenes de productos y categorias automaticamente

--- Productos/models.py
+++ Productos/models.py
@@ -2,20 +2,32 @@ from django.core.exceptions import ValidationError
 from django.db import models
 
 
+def save_image_product(instance, filename):
+    return 'productos/{}/{}'.format(instance.id, filename)
+
+
+def save_image_category(instance, filename):
+    return 'categorias/{}/{}'.format(instance.id, filename)
+
+
 class Categoria(models.Model):
     nombre = models.CharField(max_length=50)
     descripcion = models.TextField()
-    imagen = models.ImageField(upload_to='categorias', blank=True)
+    imagen = models.ImageField(upload_to=save_image_category, blank=True)
+    number_prd = models.IntegerField(default=0)
 
     def __str__(self):
         return self.nombre
+
+    def get_number_prd(self):
+        return self.producto_set.count()
 
 
 class Producto(models.Model):
     nombre = models.CharField(max_length=50)
     descripcion = models.TextField()
     precio = models.DecimalField(max_digits=10, decimal_places=2)
-    imagen = models.ImageField(upload_to='productos/', blank=True)
+    imagen = models.ImageField(upload_to=save_image_product, blank=True)
     cantidad = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True, blank=True)
