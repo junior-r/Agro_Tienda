1270e3075e0b952bb71e47ed4e0c39d916799edf
Junior Ruiz <junior31064049@gmail.com>
2022-07-11 13:19:05 -0400

    implementando iconos de pago

--- staticfiles/productos/js/main.js
+++ staticfiles/productos/js/main.js
@@ -1,1 +1,16 @@
-/home/michael/Agro_Tienda/Productos/static/productos/js/main.js
+$('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:0,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
