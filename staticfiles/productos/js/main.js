85eebe24cf48b1e1bf569de8b3ef29bb54182126
Junior <junior31064049@gmail.com>
2022-07-14 14:24:55 +0000

    base and main.js from server

--- staticfiles/productos/js/main.js
+++ staticfiles/productos/js/main.js
@@ -1,16 +1,1 @@
-$('.owl-carousel').owlCarousel({
-    loop:false,
-    margin:0,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:1
-        }
-    }
-})
+/home/michael/Agro_Tienda/Productos/static/productos/js/main.js
