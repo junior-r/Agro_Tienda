0e80dbf201fa6e495b0d02b0449af75f103559a2
Junior Ruiz <junior31064049@gmail.com>
2022-08-01 20:58:26 -0400

    eventos 3

--- Eventos/models.py
+++ Eventos/models.py
@@ -10,6 +10,7 @@ class Evento(models.Model):
     descripcion = models.TextField(max_length=400)
     imagen = models.ImageField(upload_to=save_image_event, blank=True, default='eventos/default.jpg')
     texto_boton = models.CharField(max_length=100, null=True, blank=True)
+    link = models.URLField(max_length=300, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     active = models.BooleanField(default=True)
 
@@ -17,8 +18,8 @@ class Evento(models.Model):
         return self.nombre
 
     def get_absolute_url(self):
-        host = 'http://localhost:8000'
-        return host + '/eventos/evento/' + str(self.id)
+        if self.link:
+            return self.link
 
     class Meta:
         ordering = ['-created_at']
