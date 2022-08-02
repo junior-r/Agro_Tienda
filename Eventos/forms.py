2c779ef6c542dcee82199152852a6f89309f1d47
Junior Ruiz <junior31064049@gmail.com>
2022-08-01 20:39:58 -0400

    eventos 2

--- Eventos/forms.py
+++ Eventos/forms.py
@@ -6,8 +6,9 @@ class EventoForm(forms.ModelForm):
     nombre = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'class': 'form-control'}), label='Nombre')
     descripcion = forms.CharField(max_length=400, widget=forms.Textarea(attrs={'class': 'form-control'}), label='Descripción')
     imagen = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control'}), label='Imagen', required=False)
+    texto_boton = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}), label='Texto del botón')
     activo = forms.BooleanField(widget=forms.CheckboxInput(attrs={'class': 'form-control'}), label='¿Activo?')
 
     class Meta:
         model = Evento
-        fields = ['nombre', 'descripcion', 'imagen', 'activo']
+        fields = ['nombre', 'descripcion', 'imagen', 'texto_boton', 'activo']
--- Eventos/models.py
+++ Eventos/models.py
@@ -9,6 +9,7 @@ class Evento(models.Model):
     nombre = models.CharField(max_length=200)
     descripcion = models.TextField(max_length=400)
     imagen = models.ImageField(upload_to=save_image_event, blank=True, default='eventos/default.jpg')
+    texto_boton = models.CharField(max_length=100, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     active = models.BooleanField(default=True)
 
