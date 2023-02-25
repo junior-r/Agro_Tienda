14be81f6da9a4eb76ed546cec24d4c3658814695
Junior Ruiz <junior31064049@gmail.com>
2023-02-24 20:57:04 -0500

    Update images

--- Eventos/forms.py
+++ Eventos/forms.py
@@ -6,8 +6,8 @@ class EventoForm(forms.ModelForm):
     nombre = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'class': 'form-control'}), label='Nombre')
     descripcion = forms.CharField(max_length=400, widget=forms.Textarea(attrs={'class': 'form-control'}), label='Descripción')
     imagen = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control'}), label='Imagen', required=False)
-    texto_boton = forms.CharField(max_length=100, widget=forms.URLInput(attrs={'class': 'form-control'}), label='Texto del botón')
-    link = forms.CharField(max_length=300, widget=forms.TextInput(attrs={'class': 'form-control'}), label='Link de redirección')
+    texto_boton = forms.CharField(max_length=100, widget=forms.URLInput(attrs={'class': 'form-control'}), required=False, label='Texto del botón')
+    link = forms.CharField(max_length=300, widget=forms.TextInput(attrs={'class': 'form-control'}), required=False, label='Link de redirección')
     activo = forms.BooleanField(widget=forms.CheckboxInput(attrs={'class': 'form-control'}), label='¿Activo?')
 
     class Meta:
